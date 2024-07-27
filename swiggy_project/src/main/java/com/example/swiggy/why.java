86ea1e8e93e256d3abb0d8e3cd03119506d51b8e
aaakshit <akshit.baba1100@gmail.com>
2024-07-28 04:23:58 +0530

    NO COMMIT

--- swiggy_project/src/main/java/com/example/swiggy/why.java
+++ swiggy_project/src/main/java/com/example/swiggy/why.java
@@ -1,6 +1,6 @@
 package com.example.swiggy;
 
-public class Main {
+public class why {
     public static void main(String[] args) {
         System.out.println("Hiii");
         System.out.println("NOOOOOOOOO");
